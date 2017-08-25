59f527f8ea91750e569dcab745baa79c88802b66
Volodymyr Vyshko <vvyshko@talend.com>
2017-08-25 23:40:37 +0300

    Test CD works ?

--- src/app.js
+++ src/app.js
@@ -12,6 +12,8 @@ import GithubActionCreator from './actions/GithubActionCreator';
 
 import './styles/app.css';
 
+console.log("Starting app..");
+
 const store = createStore(reducer);
 
 const enableUrlController = store => {
